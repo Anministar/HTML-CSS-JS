e0ebbb13e50fbad9c9fac250ab1afcd593dc6d92
Anministar <ldh1081703@gmail.com>
2022-12-12 17:37:59 +0900

    STUDYING_2022_12_12 MON

---
+++ 15 JS Cdn/js/02.js
@@ -0,0 +1,33 @@
+new Swiper ('section>.main-banner>.inner>.swiper', {
+direction : 'horizontal',    //수형 슬라이드(기본)
+autoplay : {                        //시작시 바로 재생 여부
+  delay : 3000,
+  disableOnInteraction : false    //사용자의 action에 따른 슬라이드 동작 정지여부
+},
+loop : true,                         //무한재생
+
+// navigation
+navigation:{
+  prevEl : 'section>.main-banner>.inner>.swiper>.swiper-button-prev' ,
+  nextEl : 'section>.main-banner>.inner>.swiper>.swiper-button-next'
+},
+//pagination
+pagination:{
+  el : '.main-banner .swiper-pagination',
+  type : 'bullets',      //bullets, progressbar, fraction (1/5 이런식으로 되어있는거)
+  clickable : 'true'
+},
+//마우스 휠 효과
+mousewheel : true,
+effect : 'fade',
+speed:1000,
+
+// 스크롤 바
+scrollbar:{
+  // el : '.main-banner .swiper-scrollbar',
+  // draggable : true
+}
+
+});
+
+
